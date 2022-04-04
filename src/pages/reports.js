3aff8bdf54c47235ac5f7033cad722a2fd83dab0
QbitCal <quenten.calvano@outlook.com>
2022-04-03 22:44:42 -0400

    Fix homepage formatting and initialize pages

--- src/pages/reports.js
+++ src/pages/reports.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import DropdownItem from 'react-bootstrap/esm/DropdownItem';
+import { Dropdown } from 'bootstrap';
+import { DropdownButton } from 'react-bootstrap';
 
 const Reports = () => {
   return (
@@ -9,8 +12,18 @@ const Reports = () => {
             height: '90vh'
         }}
     >
-        <h1>Reports</h1>
-
+        <div className='pageContent'>
+            <h1>Reports</h1>
+            <p>
+            This is the Fleur De Lis Pet Spa Reports page.
+            Once fully setup, this page will display the vailable reports with a dropdown menu allowing the user to change which report they wish to view.
+            </p>
+            {/* <DropdownButton id="reports-dropdown" title="Select Report">
+                <Dropdown.Item href="#/action-1">Appointments: Weekly</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Geographic-Visualiztion of Appointments: Weekly</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Heat-Map-Visualization of Appointments: Monthly</Dropdown.Item>
+            </DropdownButton> */}
+        </div>
     </div>
   )
 }
